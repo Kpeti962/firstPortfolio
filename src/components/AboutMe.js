d1ef2c4762c93c00b0255b2987ec07299a8e8fc8
Peti <kpeti962@gmailcom>
2022-08-29 10:36:57 +0200

    -

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -24,13 +24,20 @@ const AboutMe = () => {
         </div>
         <div className="shortStory">
           <h3>
-            I have been learning to be a frontend developer since May 2022 under
-            the influence of one of my friends, and I completed a frontend developer online training at EPAM Systems Inc. The more I delved deeper into
-            its mysteries, the more I liked this profession. I know I still have a long way to go, but I think I'm on the right track. In this portfolio I
-            try to show everything I've learned so far. 
-            <h2>
-            And the journey continues...
-            </h2>
+            I graduated in tourism and hospitality in January 2021 during the
+            covid period, so I couldn't take part in an internship and I worked
+            in a completely different position for a year after that, as an
+            administrator in a great team and already in the meantime I was
+            planning that I would have to change, because I didn't feel that
+            hospitality could bring change in my life ever and I decided to
+            learn to program. I have been learning to be a frontend developer
+            since May 2022 under the influence of one of my friends, and I
+            completed a frontend developer online training at EPAM Systems Inc.
+            The more I delved deeper into its mysteries, the more I liked this
+            profession. I know I still have a long way to go, but I think I'm on
+            the right track. In this portfolio I try to show everything I've
+            learned so far.
+            <h2>And the journey continues...</h2>
           </h3>
         </div>
       </div>
